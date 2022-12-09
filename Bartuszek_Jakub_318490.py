8c4346d32094ef1602c0f29b556899f68d69fdd0
Jakub Bartuszek <01169002@pw.edu.pl>
2022-12-09 20:44:55 +0100

    Pretty much complete, working properly

--- Bartuszek_Jakub_318490.py
+++ Bartuszek_Jakub_318490.py
@@ -1,37 +1,33 @@
 from rich.console import Console
 from rich.table import Table
-from rich.prompt import Prompt
+from rich.prompt import FloatPrompt, IntPrompt
 
-
-
-def read_data():
-    amount_of_mortgage=Prompt.ask("Enter amount of mortgage")
-    bank_fee=Prompt.ask("Enter bank fee (in per cent)")
-    interest_rate=Prompt.ask("Enter interest rate (in per cent, yearly)")
-    period_of_payment=Prompt.ask("Enter period of payment (in months)")
+def read_args():
+    amount_of_mortgage=IntPrompt.ask("Enter amount of mortgage")
+    bank_fee=FloatPrompt.ask("Enter bank fee (in per cent)")
+    interest_rate=FloatPrompt.ask("Enter interest rate (in per cent, yearly)")
+    period_of_payment=IntPrompt.ask("Enter period of payment (in months)")
     return amount_of_mortgage, bank_fee, interest_rate, period_of_payment
 
 def calculations(amount_of_mortgage, bank_fee, interest_rate, period_of_payment):
-    interest_installment=0
-    capital_installment = (amount_of_mortgage*(1+bank_fee/100))/(period_of_payment)
-    for i in range(period_of_payment):
-        interest_installment[i] = (((amount_of_mortgage*(1+bank_fee/100))-(i*capital_installment))*interest_rate/100)/12
-    return capital_installment, interest_installment
+    installment = (amount_of_mortgage*(interest_rate/100)*(1+bank_fee/100))/(12*(1-((12/(12+(interest_rate/100)))**period_of_payment)))
+    return installment
 
-def make_a_table(capital_installment, interest_installment, period_of_payment):
+def make_a_table(installment, period_of_payment):
+    sum_of_installments = 0
     table = Table(title="Mortgage installments")
 
     table.add_column("Month")
-    table.add_column("Capital Installment")
-    table.add_column("Interest Installment")
-    table.add_column("Sum of installments")
+    table.add_column("Installment")
+    table.add_column("Sum of all installments")
 
     for i in range(period_of_payment):
-        table.add_row(i,capital_installment,interest_installment,interest_installment+capital_installment)
+        sum_of_installments+=installment
+        table.add_row(str(i+1),str(round(installment,2)),str(round(sum_of_installments,2)))
     
     console = Console()
     console.print(table)
-    return
 
-read_data()
-calculations(read_data())
+amount_of_mortgage,bank_fee,interest_rate,period_of_payment = read_args()    
+
+make_a_table(calculations(amount_of_mortgage,bank_fee,interest_rate,period_of_payment),period_of_payment)
